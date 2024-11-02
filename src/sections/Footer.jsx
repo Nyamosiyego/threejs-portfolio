a9d2e173e3f0ff4544ab4fad864c9a74bacd265c
Edmond <edmond.nyamosi@strathmore.edu>
2024-11-02 22:02:05 +0300

    update

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -8,15 +8,21 @@ const Footer = () => {
       </div>
 
       <div className="flex gap-3">
+      <a href="https://github.com/Nyamosiyego">
         <div className="social-icon">
           <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
         </div>
-        <div className="social-icon">
-          <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
-        </div>
-        <div className="social-icon">
-          <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
-        </div>
+        </a>
+        <a href="https://x.com/EdmondIts">
+          <div className="social-icon">
+            <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
+          </div>
+        </a>
+        <a href="https://www.linkedin.com/in/edmond-nyamosi-0150b0163/">
+          <div className="social-icon">
+            <img src="/assets/linkedin.svg" alt="instagram" className="w-1/2 h-1/2" />
+          </div>
+        </a>
       </div>
 
       <p className="text-white-500">© 2024 Edmond Nyamosi. All rights reserved.</p>
